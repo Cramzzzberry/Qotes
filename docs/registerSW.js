7dab9206ddce841320085e3b4d5e8b8a55464b16
Cramzzzberry <janroebantuan@gmail.com>
2024-02-12 14:48:33 +0800

    Build #1 1.1 - Base url on vite config now added

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/qotes/sw.js', { scope: '/qotes/' })})}
