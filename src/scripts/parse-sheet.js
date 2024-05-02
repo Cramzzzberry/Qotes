6149280d58e355f2898f0b3f027684f8fcea8e25
Cramzzzberry <janroebantuan@gmail.com>
2024-05-02 21:19:23 +0800

    Fix: Chords doesnt parse when Info syntax is used in the same line

--- src/scripts/parse-sheet.js
+++ src/scripts/parse-sheet.js
@@ -20,20 +20,31 @@ export default function parseSheet(input) {
     } else if (isSongSection(str)) {
       parsedStr = str.replace(/>> /, '');
       parsedLineOfStrs.push(`<div class="song-section">${parsedStr}</div>`);
-    } else if (isInfo(str)) {
-      //if it is an info
-      parsedStr = str.replace(/\[/, '<span class="info">');
-      parsedStr = parsedStr.replace(/\]/, '</span>');
-      parsedLineOfStrs.push(`<div>${parsedStr}</div>`);
     } else {
       //if there is no prefix or one liner syntax like --- for hr
       let parsedPhrases = [];
       let phrases = str.replace(/ /g, '|&nbsp|').split('|');
+      let tempInfohead = null;
 
       phrases.forEach((songEntity) => {
-        if (isChord(songEntity)) {
+        if (isChord(songEntity) && !tempInfohead) {
           //if it is a chord
           parsedPhrases.push(`<span class="chord">${songEntity}</span>`);
+        } else if (isInfo(songEntity) && !tempInfohead) {
+          //if it is a info
+          if (songEntity.split('').pop() === '*') {
+            songEntity = songEntity.replace(/\*/g, '');
+            parsedPhrases.push(`<span class="info">${songEntity}</span>`);
+          } else {
+            songEntity = songEntity.replace(/\*/, '');
+            tempInfohead = `<span class="info">${songEntity}`;
+          }
+        } else if (isInfo(songEntity) && tempInfohead) {
+          songEntity = songEntity.replace(/\*/, '');
+          parsedPhrases.push(tempInfohead + `${songEntity}</span>`);
+          tempInfohead = null;
+        } else if (tempInfohead) {
+          tempInfohead = tempInfohead + songEntity;
         } else {
           //if it is a lyrics
           parsedPhrases.push(songEntity);
@@ -64,5 +75,5 @@ function isSongSection(str) {
 }
 
 function isInfo(str) {
-  return /\[.+\]/.test(str);
+  return /(\*.+(\*?))|(.+\*)/.test(str);
 }
